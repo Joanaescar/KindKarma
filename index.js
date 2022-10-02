dece37dcec1dc1c76da02d3d4a0808ea995484e3
Joanaescar <joanaa-esteves@hotmail.com>
2022-10-02 23:15:49 +0100

    Base project setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+})
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
