36cbc63119bd59809af3bf9d9f56c22a84e5410d
Joanaescar <joanaa-esteves@hotmail.com>
2022-10-05 20:03:33 +0100

    Navbar progress
    -Added menu dropdown options
    - Authentication modals

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
